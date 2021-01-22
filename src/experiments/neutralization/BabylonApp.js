d0708b10b96ecd8bce4f82eac79b2ad28a031506
Ethan <zc15372090638@gmail.com>
2021-01-22 19:24:52 +0800

    从acid_vue迁移完成

--- src/experiments/neutralization/BabylonApp.js
+++ src/experiments/neutralization/BabylonApp.js
@@ -1,6 +1,7 @@
 import * as BABYLON from '@babylonjs/core/Legacy/legacy'
 import '@babylonjs/loaders/glTF'
 import createScene from './babylon/createScene'
+import animationBox from './babylon/animationBox'
 
 export default class BabylonApp {
   constructor(domId) {
@@ -18,30 +19,41 @@ export default class BabylonApp {
   }
 
   pullInCamera() {
-    const camera = this.scene.activeCamera
-    const frameRate = 12
-    const pullInCamera = new BABYLON.Animation(
-      'pullInCamera',
-      'position',
-      frameRate,
-      BABYLON.Animation.ANIMATIONTYPE_VECTOR3,
-      BABYLON.Animation.ANIMATIONLOOPMODE_CONSTANT
-    )
-    const pullInCameraFrames = []
-    pullInCameraFrames.push({
-      frame: 0,
-      value: camera.position
-    })
-    pullInCameraFrames.push({
-      frame: 6 * frameRate,
-      value: new BABYLON.Vector3(0, 50, -50)
-    })
-    pullInCamera.setKeys(pullInCameraFrames)
-    camera.setTarget(new BABYLON.Vector3(0, 60, 0))
-    setTimeout(() => {
-      this.scene.beginDirectAnimation(camera, [pullInCamera], 0, 6 * frameRate, false, 6, function() {
-        console.log('完成！')
+    return new Promise((resolve, reject) => {
+      const frameRate = 12
+      const pullInCamera = animationBox.moveCameraByPosition(
+        this.scene.activeCamera,
+        new BABYLON.Vector3(0, 50, -50),
+        6 * frameRate
+      )
+      const showDropper = animationBox.showMesh(2 * frameRate)
+      this.scene.activeCamera.setTarget(new BABYLON.Vector3(0, 60, 0))
+      this.scene.beginDirectAnimation(this.scene.activeCamera, [pullInCamera], 0, 6 * frameRate, false)
+
+      this.scene.beginDirectAnimation(this.scene.getMeshByName('dropper_primitive0'), [showDropper], 0, 2 * frameRate, false)
+      this.scene.beginDirectAnimation(this.scene.getMeshByName('dropper_primitive1'), [showDropper], 0, 2 * frameRate, false)
+      this.scene.beginDirectAnimation(this.scene.getMeshByName('dropliquid'), [showDropper], 0, 2 * frameRate, false, 1, () => {
+        resolve()
       })
-    }, 500)
+    })
+  }
+
+  firstDropLiqiud() {
+    return new Promise((resolve, reject) => {
+      const frameRate = 12
+      const liquidSphere = this.scene.getMeshByName('liquidSphere')
+
+      this.scene.beginDirectAnimation(
+        liquidSphere,
+        [animationBox.liquidSphereVisible, animationBox.liquidScale, animationBox.dropLiquid],
+        0,
+        2 * frameRate,
+        false,
+        1,
+        () => {
+          resolve()
+        }
+      )
+    })
   }
 }
