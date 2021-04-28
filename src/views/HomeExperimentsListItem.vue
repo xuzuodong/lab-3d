6f5f5a9de9ee4b0e2fe98be0431e3559a4090e02
Ethan <zc15372090638@gmail.com>
2021-04-28 21:30:01 +0800

    首页单个实验区域展示

--- src/views/HomeExperimentsListItem.vue
+++ src/views/HomeExperimentsListItem.vue
@@ -18,31 +18,30 @@
         </q-card-section>
       </q-card>
     </router-link>
-    <div v-if="experiment.alias == null && userInfo != null">
-      <a target="_blank" :href="'http://47.98.192.17/' + oldExperiment + '.html' + '?' + userInfo.token">
-        <q-card class="experiment-item cursor-pointer full-height">
-          <q-img :src="experiment.image" width="100%" :ratio="3 / 2" />
+    <div v-if="experiment.alias == null" @click.prevent="openOldExperiment()">
+      <q-card class="experiment-item cursor-pointer full-height">
+        <q-img :src="experiment.image" width="100%" :ratio="3 / 2" />
 
-          <q-card-section class="row items-top no-wrap">
-            <div class="text-h6">
-              {{ experiment.name }}
-              <q-badge color="yellow-6" text-color="black">
-                {{ experiment.subject }}
-              </q-badge>
-            </div>
-          </q-card-section>
+        <q-card-section class="row items-top no-wrap">
+          <div class="text-h6">
+            {{ experiment.name }}
+            <q-badge color="yellow-6" text-color="black">
+              {{ experiment.subject }}
+            </q-badge>
+          </div>
+        </q-card-section>
 
-          <q-card-section class="q-pt-none" v-snip:js="4">
-            {{ experiment.description }}
-          </q-card-section>
-        </q-card>
-      </a>
+        <q-card-section class="q-pt-none" v-snip:js="4">
+          {{ experiment.description }}
+        </q-card-section>
+      </q-card>
     </div>
   </div>
 </template>
 
 <script>
 import { mapState } from 'vuex'
+import DialogJoinVue from '../components/DialogJoin.vue'
 export default {
   props: { experiment: Object },
   computed: {
@@ -51,6 +50,20 @@ export default {
       if (this.experiment.id === 1) return 'saltpage'
       if (this.experiment.id === 2) return 'phpage'
       return 'homepage'
+    },
+  },
+  methods: {
+    openOldExperiment() {
+      if (this.userInfo != null) {
+        window.open(`http://47.98.192.17/${this.oldExperiment}.html?${this.userInfo.token}`, '_blank')
+      } else {
+        this.$q
+          .dialog({
+            component: DialogJoinVue,
+            parent: this,
+          })
+          .onOk(() => {})
+      }
     },
   },
 }
